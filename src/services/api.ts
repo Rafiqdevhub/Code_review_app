22f256e2beedb5fa49af31eda31b23881fc8d1ba
Rafiqdevhub <rafiq85825@gmail.com>
2025-09-20 20:04:52 +0500

    Correct the backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ import type {
   ChangePasswordRequest,
 } from "@/types/auth";
 
-const API_BASE_URL = "https://codereviewbackend-sigma.vercel.app";
+const API_BASE_URL = "https://codify-backend-sigma.vercel.app";
 // const API_BASE_URL = "http://localhost:5000";
 const isDevelopment = import.meta.env.DEV;
 const isMockMode = import.meta.env.VITE_MOCK_API === "true";
